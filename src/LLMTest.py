a5a6ea8eef1c99d302a49f198c3655780ad25b3a
Julian <julekww@op.pl>
2025-04-03 11:55:40 +0200

    API Test

---
+++ src/LLMTest.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+openai_key = '<API KEY>'
+
+client = OpenAI(api_key=openai_key)
+
+response = client.responses.create(
+    model="o3-mini-2025-01-31",
+    input="Write a one-sentence bedtime story about a unicorn."
+)
+
+print(response.output_text)
+
+
+
